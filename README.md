b6428231ae725ebb94433b58febdf6c3bc067d06
Maryia Radchuk <maryia.radchuk@just-eat.com>
2019-08-21 16:40:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Vue app for testing webpack tree shaking
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Build in dev mode
+```
+yarn build-dev
+```
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <LibComponentOne />
+  </div>
+</template>
+
+<script>
+import { LibComponentOne } from 'vue-library-sample'
+
+export default {
+  name: 'app',
+  components: {
+    LibComponentOne
+  }
+}
+</script>
